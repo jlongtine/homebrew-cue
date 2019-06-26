9ea07a8702e6f53679d2716817804cfc170867c1
Joel Longtine <jlongtine@tangogroup.com>
2019-06-26 09:22:00 -0600

    Add cue version

--- cue.rb
+++ cue.rb
@@ -3,6 +3,7 @@ class Cue < Formula
   homepage "https://github.com/cuelang/cue"
   url "https://github.com/cuelang/cue/releases/download/v0.0.3/cue_0.0.3_Darwin_x86_64.tar.gz"
   sha256 "96100fe1566c79ae6f2c68606149c0ccc10fb114b19db491bec154efa88e4e4f"
+  version "0.0.3"
 
   def install
     bin.install "cue"
