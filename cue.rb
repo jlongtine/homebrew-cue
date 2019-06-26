2938ea05452418ac135e897dff8760d659881ee1
Joel Longtine <jlongtine@tangogroup.com>
2019-06-26 09:17:12 -0600

    Fix SHA256

--- cue.rb
+++ cue.rb
@@ -2,7 +2,7 @@ class Cue < Formula
   desc "Manipulate INI files from the command line"
   homepage "https://github.com/cuelang/cue"
   url "https://github.com/cuelang/cue/releases/download/v0.0.3/cue_0.0.3_Darwin_x86_64.tar.gz"
-  sha256 "6767a013c4d7e0572317f2ab78187fac70b598bf"
+  sha256 "96100fe1566c79ae6f2c68606149c0ccc10fb114b19db491bec154efa88e4e4f"
 
   def install
     bin.install "cue"
