1c19d12a8e4aecc8d7781dbfba4c3bdf4d4b7690
Joel Longtine <jlongtine@tangogroup.com>
2019-06-27 10:25:16 -0600

    Change description

--- cue.rb
+++ cue.rb
@@ -1,5 +1,5 @@
 class Cue < Formula
-  desc "Manipulate INI files from the command line"
+  desc "CUE is an open source data constraint language which aims to simplify tasks involving defining and using data."
   homepage "https://github.com/cuelang/cue"
   url "https://github.com/cuelang/cue/releases/download/v0.0.3/cue_0.0.3_Darwin_x86_64.tar.gz"
   sha256 "96100fe1566c79ae6f2c68606149c0ccc10fb114b19db491bec154efa88e4e4f"
